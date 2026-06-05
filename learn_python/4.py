3e5d0d60bec3e3a707e3429e48e91c061add2729
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-05 14:59:53 +0530

    practise

--- learn_python/4.py
+++ learn_python/4.py
@@ -1,4 +1,3 @@
-year = int(input("Enter a year: "))
 # divided by 100 means century year (ending with 00)
 # century year divided by 400 is leap year
 if (year % 400 == 0) and (year % 100 == 0):
---
+++ learn_python/Subtraction.py
@@ -0,0 +1,9 @@
+def subtract_numbers(a, b):
+    return a - b
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = subtract_numbers(num1, num2)
+
+print("Subtraction =", result)
