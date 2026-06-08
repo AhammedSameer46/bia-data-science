27b1a4dd94fff69aac9b19f1e217524fb6e50050
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-08 22:12:00 +0530

    python practise session

---
+++ learn_python/even_numbers.py
@@ -0,0 +1,10 @@
+for i in range(1,21):
+    if i%2==0:
+        print(i)
+
+
+
+
+##version 2 without if statement
+for i in range(2, 21, 2):
+    print(i)
---
+++ learn_python/multiplication_table.py
@@ -0,0 +1,3 @@
+num=float(input("ENter a number:"))
+for i in range(1,11):
+    print(f"{num} x {i} = {num*i}")
