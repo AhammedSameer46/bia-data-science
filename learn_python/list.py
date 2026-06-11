7cacc0f8a482d777dfb2b818be0d1fe8c7d27045
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-11 14:04:40 +0530

    mastering list

---
+++ learn_python/list.py
@@ -0,0 +1,29 @@
+students=["Ahammed","Sameer","Ahad",]
+#print(students)
+students[2]="jasneem"
+#print(students)
+
+## apend()
+
+fruits=["apple","banana","orange"]
+fruits.append("green apple")
+#print(fruits)
+
+#insert()
+students1=["Ahammed","Ahad",]
+students1.insert(1,"sameer")
+#print(students1)
+
+#remove()
+students1=["Ahammed","Ahad",]
+students1.remove("Ahad")
+#print(students1)
+
+#pop()
+students1=["Ahammed","Ahad",]
+students1.pop(1)
+#print(students1)
+
+#len()
+students1=["Ahammed","Ahad",]
+print(len(students1))
