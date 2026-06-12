065ef4711d46dd05c147b707e0a4cbb6e474ae87
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-12 21:56:26 +0530

    dict study

---
+++ learn_python/dictionary.py
@@ -0,0 +1,12 @@
+students={
+    "name":"Ahammed",
+    "age":25,
+    "city":"Kerala",
+    "country":"India",
+    "hobbies":["coding","traveling","cooking"],
+    "education":"Bachelor's in Computer Science",
+    "bike":"ktm",
+}
+
+
+print(students["name"])
