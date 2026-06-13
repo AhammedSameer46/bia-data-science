4d18890ecb89a3be9409fd8217735ca9bd02b5c5
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-13 22:48:53 +0530

    def ver 2

--- learn_python/def.py
+++ learn_python/def.py
@@ -13,3 +13,43 @@ result = add(10,20)
 print("The sum of two numbers is:",result)
 
 
+def square(num):
+    return num*num
+
+result = square(5)
+print("The square of the number is:",result)
+
+
+
+
+
+def student_info(name,age,course):
+    print("My name is ",name,"and my age is ",age,"and I am studying ",course)
+
+
+    
+student_info("Ahammed",21,"computer science")
+
+
+
+
+## looping through a def
+
+def print_numbers():
+    for i in range(1,11):
+        print(i)
+
+print_numbers()
+
+
+
+
+### def list
+
+def show_students():
+    students = ["Ahammed", "Ali", "Ahmed", "Abdul"]
+
+    for student in students:
+        print(student)
+
+show_students()
