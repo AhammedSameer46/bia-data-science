e35b6e93c95818edfb1a0aacdac1f9af23026733
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-13 22:28:38 +0530

    Def funcation

---
+++ learn_python/def.py
@@ -0,0 +1,15 @@
+def greet(name):
+    print("My name is",name,"i am a python programmer")
+
+greet("Ahammed")
+
+
+## Addition of two numbers
+
+def add(a,b):
+    return a+b
+
+result = add(10,20)
+print("The sum of two numbers is:",result)
+
+
