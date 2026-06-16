a95e9b48425bea76230ab6408319a8005cbcf1f7
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-16 21:50:51 +0530

    Append in file handling

--- file_handling/read_file.py
+++ file_handling/read_file.py
@@ -8,3 +8,12 @@ print(content)
 ## TO Write a File
 with open ("file_handling/student.txt",'w') as file:
         file.write("Ahad")
+
+
+
+## Append (a)
+
+with open ("file_handling/student.txt",'a') as file:
+        file.write("\nComputer Science")
+
+
