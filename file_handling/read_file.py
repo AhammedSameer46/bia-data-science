1245b8768fbebc5ce88898d3ccbcf548b9f229c2
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-16 21:36:02 +0530

    write file

--- file_handling/read_file.py
+++ file_handling/read_file.py
@@ -1,3 +1,10 @@
 with open("file_handling/student.txt",'r') as file:
     content=file.read()
 print(content)
+
+
+
+
+## TO Write a File
+with open ("file_handling/student.txt",'w') as file:
+        file.write("Ahad")
