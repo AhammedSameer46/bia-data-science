01fffe647ee4b74042421f70ccfc2fbd7bdfbc47
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-17 13:33:48 +0530

    pattern practise

---
+++ learn_python/pattern.py
@@ -0,0 +1,15 @@
+"""for i in range (5):
+    print("*")"""
+
+"""for i in range (5):
+    print(i)"""
+
+
+'''for latter in  ["A","B","c","D","E"]:
+    print(latter)'''
+
+
+for row in range (5): 
+        for col in range(5):
+            print("*",end="")
+        print()
