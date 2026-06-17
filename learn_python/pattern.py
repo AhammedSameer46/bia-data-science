bb4fa9788d8cf602a24208dcca47d55b7d56484f
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-17 19:30:07 +0530

    patters cleared

--- learn_python/pattern.py
+++ learn_python/pattern.py
@@ -25,3 +25,26 @@ for row in range(5):
     for col in range(5-row):
         print("*",end="")
     print()'''
+
+
+
+'''for row in range(1,6):
+    for col in range(1,row+1):
+        print(col,end="")
+    print()'''
+
+
+
+'''for row in range(1, 6):
+    for col in range(1, row + 1):
+        print(row,end="")
+    print()'''
+
+
+
+
+
+'''for row in range(1,7):
+    for col in range(1,7-row):
+        print(col,end="")
+    print()'''
