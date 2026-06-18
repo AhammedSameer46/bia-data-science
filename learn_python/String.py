e8f37f395e3a4be9d19cf8377df60127ec2b8b76
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-18 20:12:38 +0530

    Basic string methods

--- learn_python/String.py
+++ learn_python/String.py
@@ -3,7 +3,19 @@ print(words[3])
 '''
 ## String Slicing
 
+
 '''name="Ahammedsameer"
 print(name[0:3])'''
 
 
+## String method
+
+name="Ahammedsameer"
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.capitalize())
+print(name.find("A"))
+print(name.count("m"))
+print(len(name))
+print(name.replace("Ahammedsameer","Ahadsameer"))
