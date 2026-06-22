4ad993433a6dd322db5a3b85650e70d7a4226764
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-22 19:26:24 +0530

    Fixed Error

--- learn_python/armstrong.py
+++ learn_python/armstrong.py
@@ -1,17 +1,20 @@
 num = int(input("Enter a number: "))
+
 # Calculate the number of digits in num
 num_str = str(num)
 num_digits = len(num_str)
+
 # Initialize variables
 sum_of_powers = 0
 temp_num = num
 
 while temp_num > 0:
-digit = temp_num % 10
-sum_of_powers += digit ** num_digits
-temp_num //= 10
+    digit = temp_num % 10
+    sum_of_powers += digit ** num_digits
+    temp_num //= 10
+
 # Check if it's an Armstrong number
 if sum_of_powers == num:
-        print(f"{num} is an Armstrong number.")
+    print(f"{num} is an Armstrong number.")
 else:
-        print(f"{num} is not an Armstrong number.")
+    print(f"{num} is not an Armstrong number.")
