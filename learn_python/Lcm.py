d74de3148562c030fa82b5b35d5888896f90aa0e
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-06-22 19:25:11 +0530

    Fixedd The indentation

--- learn_python/Lcm.py
+++ learn_python/Lcm.py
@@ -1,15 +1,20 @@
 def compute_lcm(x, y):
-    if x > y:                    
-# choose the greater number
-    greater = x
-else:
-greater = y
-while(True):
-if((greater % x == 0) and (greater % y == 0)):
-lcm = greater
-break
-greater += 1
-return lcm
-num1 = int(input('Enter the number: '))
-num2 = int(input('Enter the number: '))
-print("The L.C.M. is", compute_lcm(num1, num2))
+    # choose the greater number
+    if x > y:
+        greater = x
+    else:
+        greater = y
+
+    while True:
+        if (greater % x == 0) and (greater % y == 0):
+            lcm = greater
+            break
+        greater += 1
+
+    return lcm
+
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("The LCM is:", compute_lcm(num1, num2))
