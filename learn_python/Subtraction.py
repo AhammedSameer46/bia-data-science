fa9e1641dae86363704a9a014b43e8288ed1fa96
Ahammed Sameer <ahammedachu1961@gmail.com>
2026-08-02 22:58:28 +0530

    Delete

--- learn_python/Subtraction.py
+++
@@ -1,9 +0,0 @@
-def subtract_numbers(a, b):
-    return a - b
-
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-result = subtract_numbers(num1, num2)
-
-print("Subtraction =", result)
